53569b47b2d9f230986c2bae1018e22ba4a18cbc
Elger Mensonides <elgerm@gmail.com>
2023-11-28 01:05:50 +0700

    initial commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp2;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore.Migrations.Internal;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+// Path: ConsoleApp2/Program.cs
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+builder.Services
+             .AddDbContext<SoiFinDb>(options =>
+             {
+                 options.ConfigureWarnings(w => w.Ignore(CoreEventId.RowLimitingOperationWithoutOrderByWarning));
+                 options.UseNpgsql("Host=rain.db.elephantsql.com;Port=5432;Database=xxtojalv;Username=xxtojalv;Password=4YFPrF_0g9M2GmPFC_qqeXi2A8VA_M3y;Include Error Detail=true").UseSnakeCaseNamingConvention();
+             });
+
+using IHost host = builder.Build();
+
+using IServiceScope scope = host.Services.CreateScope();
+
+var db = scope.ServiceProvider.GetRequiredService<SoiFinDb>();
+
+var list = await db.TestTables.Take(10).ToListAsync();
+
+foreach (var item in list)
+{
+    Console.WriteLine($"{item.Created} kind: {item.Created.Kind}");
+}
+
+Console.ReadLine();
---
+++ ConsoleApp2/SoiFinDb.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace ConsoleApp2
+{
+
+    public class SoiFinDb : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public string Hostname { get; set; }
+
+        private readonly IConfiguration configuration;
+
+        public SoiFinDb(DbContextOptions<SoiFinDb> options, IConfiguration configuration) : base(options)
+        {
+            this.configuration = configuration;
+        }
+        public DbSet<TestTable> TestTables { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+    [Table("test_table")]
+    public class TestTable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long MoneyTransactionId { get; set; }
+        public int Amount { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? ExpiryDate { get; set; }
+    }
+
+}
+
